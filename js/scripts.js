93781d182f4fe9d4daac7cb80e262d6c1068177d
Cristina Turcanu <turcanu.cristina.v@gmail.com>
2018-08-28 17:17:40 +0300

    add product details

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 $(document).ready(() => {
-  categoryContainer = $(".products .categories");
-  let categoryUrl =
-    "http://iw-internship.herokuapp.com/api/v1/furniture-categories/";
+
+  categoryContainer = $('.products .categories');
+  let categoryUrl = "http://iw-internship.herokuapp.com/api/v1/furniture-categories/";
   fetch(categoryUrl)
   .then(response => {
     return response.json();
@@ -11,47 +11,148 @@ $(document).ready(() => {
       if(category.visible) {
         btn = $('<button class=btn/>');
         btn.text(category.name);
-        // btn.val(category.id);
         btn.attr('data-id', category.id);
         categoryContainer.append(btn);
       }
-    });
+    }
+  });
 
-  $(document).on("click", ".products .categories .btn", evt => {
+  $(document).on('click', '.products .categories .btn', (evt) => {
     let target = $(evt.target);
+    const catId = target.data("id");
 
-    furnituresUrl = `${categoryUrl}${target.data("id")}/furnitures`;
+    furnituresUrl = `${categoryUrl}${catId}/furnitures`;
 
     fetch(furnituresUrl)
-      .then(response => {
+      .then((response) => {
         return response.json();
       })
 
-    .then( furnitures => {
-        cardContainer = $(".products .cards");
-        cardContainer.children(".card.cln").remove();
-        cardTemplate = cardContainer.find(".card");
-        for (furniture of furnitures) {
-            if (furniture.visible) {
-                let card = cardTemplate.clone()
-                card.prop('hidden', false);
-                card.attr('id', furniture.position);
-                card.addClass('cln');
-                card.find('.description .name').text(furniture.name)
-                card.find('.description .price').text(furniture.price + ' $')
-                card.find('.description .availability').text(furniture.availability)                  
-                card.find('.description .descr').text(furniture.description)
-                cardContainer.append(card)
+      .then( furnitures => {
+          cardContainer = $(".products .cards");
+          cardContainer.children('.card').remove();
+          // cardTemplate = cardContainer.find('.card');
+          for (furniture of furnitures) {
+              if (furniture.visible) {
+                
+                  // let card = cardTemplate.clone()
+                  // card.prop('hidden', false);
+                  // card.attr('data-id', furniture.id);
+                  // card.addClass('cln');
+                  // card.find('.description .name').text(furniture.name)
+                  // card.find('.description .price').text(furniture.price + ' $')
+                  // card.find('.description .availability').text(furniture.availability)                  
+                  // card.find('.description .descr').text(furniture.description)
+                  cardContainer.append(generateCard(furniture, catId));
+              }
+          }
+      });
+    });
+
+    $('.products .cards .card .description .name').click(function() {
+      const queryParamsString = window.location.search.substr(1);
+      const queryParams = queryParamsString
+        .split('&')
+        .reduce((accumulator, singleQueryParam) => {
+          const [key, value] = singleQueryParam.split('=');
+          accumulator[key] = decodeURIComponent(value);
+          return accumulator;
+        }, {});
+    
+        let categoryId = queryParams["cid"];
+        let furnitureId = queryParams["fid"]; 
+        if(categoryId && furnitureId) {
+          let productDetailUrl = 'http://iw-internship.herokuapp.com/api/v1/furniture-categories/${categoryId}/furnitures/${furnitureId}';
+          fetch(productDetailUrl)
+          .then(response => {
+            return response.json();
+          })
+          .then(productDetail => {
+            if(productDetail.visible) {
+              let details = 
+              `<section class="prod-details">
+                  <div class="image">
+                      <img src="images/products/living2.jpg" alt="">
+                  </div>
+                  <div class="details">
+                      <h3 class="name">${productDetail.name}</h3>
+                      <hr>
+                      <p class="price">${productDetail.price}</p>
+                      <p class="description">${productDetail.description}</p>
+                      <p class="color">${'Color: ' + productDetail.color}</p>
+                      <p class="availability">Availability: ${productDetail.availability}</p>
+      
+                      <div class="shop">
+                          <select>
+                              <option>1</option>
+                              <option>2</option>
+                          </select>
+                          <button>Add to cart</button>
+                      </div>
+                  </div>
+                </section>`
+            $('.products .cards').append(details);
             }
+          }); 
         }
     });
   });
 
-  $('.products .cards .card').click(function(id) {
-    var productUrl = '';
-    $(location).attr('href', productUrl);
-  });
-});
+
+function generateCard(furniture, catId) {
+  return `<div class="card">
+            <img src="images/products/bed1.jpg" alt="">
+            <div class="description">
+                <a href="product-details.html?cid=${catId}&fid=${furniture.id}"><p class="name">${furniture.name}</p></a>
+                <p class="price">${furniture.price + ' $'}</p>
+                <p class="availability">${furniture.availability}</p>
+                <p class="descr">${furniture.description}</p>
+            </div>
+            <button class="add">Add to cart</button>
+            <div class="overlay">
+                <i class="fa fa-heart"></i> 
+            </div>
+        </div> `;
+  
+}
+// function showDetails(productDetail) {
+//   return `<section class="prod-details">
+//             <div class="image">
+//                 <img src="images/products/living2.jpg" alt="">
+//             </div>
+//             <div class="details">
+//                 <h3 class="name">${productDetail.name}</h3>
+//                 <hr>
+//                 <p class="price">${productDetail.price}</p>
+//                 <p class="description">${productDetail.description}</p>
+//                 <p class="color">${'Color: ' + productDetail.color}</p>
+//                 <p class="availability">Availability: ${productDetail.availability}</p>
+
+//                 <div class="shop">
+//                     <select>
+//                         <option>1</option>
+//                         <option>2</option>
+//                     </select>
+//                     <button>Add to cart</button>
+//                 </div>
+//             </div>
+//           </section>`;
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -72,4 +173,3 @@ $(document).ready(() => {
 //     $('.card').show(500);
 //   });
 // });
-
